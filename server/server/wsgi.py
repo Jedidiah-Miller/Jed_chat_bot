a73a92f3172032242dba2a19a13abeb754fbf45a
Jedidiah-Miller <jedidiah.a.miller@gmail.com>
2019-09-16 16:29:55 -0700

    gave up on azue and now it's back to AWS EC2

--- server/server/wsgi.py
+++ server/server/wsgi.py
@@ -11,6 +11,13 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+def ENV(is_production: bool):
+  '''
+  return the desired settings for the server ( development or production )
+  '''
+  return 'production' if is_production else 'development'
+# change is_producion to True before deploying
+is_production = True
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings.' + ENV(is_production))
 
 application = get_wsgi_application()
